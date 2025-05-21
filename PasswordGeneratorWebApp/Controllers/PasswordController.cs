4673682acd66ca7be4b25fa0e69bd606810a52e4
Pathum Kularathna <virajithapathum2001@gmail.com>
2025-05-21 13:23:05 +0530

    Update PasswordController.cs

--- PasswordGeneratorWebApp/Controllers/PasswordController.cs
+++ PasswordGeneratorWebApp/Controllers/PasswordController.cs
@@ -3,53 +3,27 @@ using PasswordGeneratorWebApp.Models;
 
 namespace PasswordGeneratorWebApp.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("[controller]")]
     public class PasswordController : ControllerBase
     {
-        [HttpPost("generate")]
+        [HttpPost]
         public ActionResult<string> GeneratePassword([FromBody] PasswordModel model)
         {
-            if (model.Length < 1)
-            {
-                return BadRequest("Password length must be at least 1.");
-            }
+            if (model.Length <= 0)
+                return BadRequest("Invalid password length.");
 
-            var password = GenerateRandomPassword(model.Length, model.IncludeUppercase, model.IncludeNumbers, model.IncludeSpecialCharacters);
-            return Ok(password);
-        }
-
-        private string GenerateRandomPassword(int length, bool includeUppercase, bool includeNumbers, bool includeSpecialCharacters)
-        {
-            const string lowercase = "abcdefghijklmnopqrstuvwxyz";
-            const string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            const string numbers = "0123456789";
-            const string specialCharacters = "!@#$%^&*()_-+=<>?";
-
-            string characterSet = lowercase;
-
-            if (includeUppercase)
-            {
-                characterSet += uppercase;
-            }
-            if (includeNumbers)
-            {
-                characterSet += numbers;
-            }
-            if (includeSpecialCharacters)
-            {
-                characterSet += specialCharacters;
-            }
+            // Simple password generation logic for demo
+            var chars = "abcdefghijklmnopqrstuvwxyz";
+            if (model.IncludeUppercase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            if (model.IncludeNumbers) chars += "0123456789";
+            if (model.IncludeSpecialCharacters) chars += "!@#$%^&*";
 
             var random = new Random();
-            char[] password = new char[length];
+            var password = new string(Enumerable.Repeat(chars, model.Length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
 
-            for (int i = 0; i < length; i++)
-            {
-                password[i] = characterSet[random.Next(characterSet.Length)];
-            }
-
-            return new string(password);
+            return Ok(password);
         }
     }
 }
