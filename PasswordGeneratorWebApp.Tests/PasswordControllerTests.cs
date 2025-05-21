7f4e374974f09aacf47693ee1baab5f94e154dbc
Pathum Kularathna <virajithapathum2001@gmail.com>
2025-05-21 14:16:26 +0530

    Update PasswordControllerTests.cs

--- PasswordGeneratorWebApp.Tests/PasswordControllerTests.cs
+++ PasswordGeneratorWebApp.Tests/PasswordControllerTests.cs
@@ -7,59 +7,9 @@ namespace PasswordGeneratorWebApp.Tests
 {
     public class PasswordControllerTests
     {
-        private readonly PasswordController _controller;
-
-        public PasswordControllerTests()
-        {
-            _controller = new PasswordController();
-        }
-
         [Fact]
         public void GeneratePassword_ValidModel_ReturnsGeneratedPassword()
         {
-            // Arrange
-            var model = new PasswordModel
-            {
-                Length = 12,
-                IncludeUppercase = true,
-                IncludeNumbers = true,
-                IncludeSpecialCharacters = true
-            };
-
-            // Act
-            var result = _controller.GeneratePassword(model);
-
-            // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result.Result);
-            var password = Assert.IsType<string>(okResult.Value);
-            Assert.False(string.IsNullOrEmpty(password));
-            Assert.Equal(12, password.Length);
-        }
-
-        [Fact]
-        public void GeneratePassword_InvalidModel_ReturnsErrorView()
-        {
-            // Arrange
-            var model = new PasswordModel
-            {
-                Length = 0, // Invalid length
-                IncludeUppercase = true,
-                IncludeNumbers = true,
-                IncludeSpecialCharacters = true
-            };
-
-            // Act
-            var result = _controller.GeneratePassword(model);
-
-            // Assert
-            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
-            Assert.Equal("Invalid password length.", badRequestResult.Value);
-        }
-
-        [Fact]
-        public void GeneratePassword_ValidRequest_ReturnsOkResultWithPassword()
-        {
-            // Arrange
             var controller = new PasswordController();
             var model = new PasswordModel
             {
@@ -71,11 +21,35 @@ namespace PasswordGeneratorWebApp.Tests
 
             // Act
             var result = controller.GeneratePassword(model);
-            var okResult = Assert.IsType<ActionResult<string>>(result);
-            string password = okResult.Value;
 
             // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewData["Error"]);
+            Assert.NotNull(viewResult.ViewData["GeneratedPassword"] ?? controller.ViewBag.GeneratedPassword);
+            var password = controller.ViewBag.GeneratedPassword as string;
             Assert.False(string.IsNullOrEmpty(password));
+            Assert.Equal(12, password.Length);
+        }
+
+        [Fact]
+        public void GeneratePassword_InvalidModel_ReturnsErrorView()
+        {
+            var controller = new PasswordController();
+            var model = new PasswordModel
+            {
+                Length = 0,
+                IncludeUppercase = true,
+                IncludeNumbers = true,
+                IncludeSpecialCharacters = true
+            };
+
+            // Act
+            var result = controller.GeneratePassword(model);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("Invalid password length.", controller.ViewBag.Error);
+            Assert.Null(controller.ViewBag.GeneratedPassword);
         }
     }
 }
